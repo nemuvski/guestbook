c6e6797cae894143fb3fb6e63df5806fbd02eb56
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-08-23 00:43:33 +0900

    TypeORM導入 & MongoDBとの接続設定

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
-import { PORT } from './constants';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 
 /**
- * Expressの設定
+ * 受け付けるポート番号
  */
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-  console.info(`🚀 Express Server running on port ${PORT}.`);
-});
+const PORT = 5678;
+
+createConnection()
+  .then(async (connection) => {
+    if (!connection.isConnected) throw new Error('Error establishing a database connection');
+
+    const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.listen(PORT, () => {
+      console.info(`🚀 Express Server running on port ${PORT}.`);
+    });
+  })
+  .catch((error) => console.error(error));
