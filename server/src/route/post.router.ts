bc00ae423bfcc15fc36f501d878b105ddf689fa8
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-08-24 00:21:52 +0900

    レスポンスに含める内容のPostのモデルを定義し、レスポンス時に詰め替える

--- server/src/route/post.router.ts
+++ server/src/route/post.router.ts
@@ -1,16 +1,31 @@
 import express from 'express';
 import { validationResult } from 'express-validator';
 import { createPost, getPosts } from '../repository/post.repository';
-import { Post } from '../entity/Post';
 import PostValidator from '../validation/post.validator';
+import { Post } from '../entity/Post';
 
 export interface IGetPostsResponsePayload {
   currentPage: number;
   nextPage?: number;
-  posts: Array<Post>;
+  posts: Array<ResponsePost>;
 }
 export interface ICreatePostRequestPayload {
   body: string;
+}
+
+/**
+ * レスポンスに含める内容のPostのモデル
+ */
+export class ResponsePost {
+  id: string;
+  body: string;
+  createdAt: Date;
+
+  constructor(post: Post) {
+    this.id = post.id.toString();
+    this.body = post.body;
+    this.createdAt = post.id.getTimestamp();
+  }
 }
 
 const router = express.Router();
@@ -34,7 +49,7 @@ router.get('/', PostValidator.getPostsValidator, async (req, res) => {
   return res.status(200).send({
     currentPage: parsedPage,
     nextPage: nextPageResults.length ? parsedPage + 1 : undefined,
-    posts: currentPageResults,
+    posts: currentPageResults.map((result) => new ResponsePost(result)),
   } as IGetPostsResponsePayload);
 });
 
@@ -47,8 +62,8 @@ router.post('/', PostValidator.createPostValidator, async (req, res) => {
     return res.status(400).send({ error: validationErrors.array() });
   }
   const payload = req.body as ICreatePostRequestPayload;
-  const results = await createPost(payload);
-  return res.status(201).send(results);
+  const result = await createPost(payload);
+  return res.status(201).send(new ResponsePost(result));
 });
 
 export default router;
